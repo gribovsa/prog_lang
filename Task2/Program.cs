dc9ab7b62434de849c1dafbf643d8be7ab6f8e4e
Gribov Sergey <gribovsa@list.ru>
2023-12-20 17:28:58 +0300

    add lesson1:task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,6 +14,13 @@ double sum3 = d1 + i1;
 int product = i1 * i2;
 long product2 = l1 * i1;
 double product3 = i1 * d1;
-Console.WriteLine(product);
-Console.WriteLine(product2);
-Console.WriteLine(product3);
+//Console.WriteLine(product);
+//Console.WriteLine(product2);
+//Console.WriteLine(product3);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
